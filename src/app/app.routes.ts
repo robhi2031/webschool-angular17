5ffd2359bbf5043a97be4b43428d98686a8b02ca
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2024-02-19 14:28:33 +0800

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PortfolioWebComponent } from './template/portfolio-web/portfolio-web.component';
+import { AdminComponent } from './template/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PortfolioWebComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+  },
+];
