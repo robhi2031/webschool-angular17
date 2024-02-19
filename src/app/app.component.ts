52a7725e30bfbf9862b2e8cca7f47b731ae8e465
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2024-02-19 14:28:32 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    MatButtonModule
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todo-sample';
